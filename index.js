bde70017c652bddeb786a991d414be8a69834793
salma <swsalama518@gmail.com>
2026-04-15 16:34:40 -0400

    Fix scope lab const requirements

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Write your solution in this file!
-let burgers = ['Hamburger', 'Cheeseburger'];
+const burgers = ['Hamburger', 'Cheeseburger'];
 let featuredDrink = 'Strawberry Milkshake';
 
 function addBurger() {
-    let newBurger = 'Flatburger';
+    const newBurger = 'Flatburger';
     burgers.push(newBurger);
 }
 if (true) {
-    let anotherNewBurger = 'Maple Bacon Burger';
+    const anotherNewBurger = 'Maple Bacon Burger';
     burgers.push(anotherNewBurger);
 }
 function changeFeaturedDrink() {
