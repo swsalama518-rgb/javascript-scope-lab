234f050f518375531be1012f18c4e1021c784f67
salma <swsalama518@gmail.com>
2026-04-15 16:29:01 -0400

    Resolve merge conflicts

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+let burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+function addBurger() {
+    let newBurger = 'Flatburger';
+    burgers.push(newBurger);
+}
+if (true) {
+    let anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+}
+function changeFeaturedDrink() {
+    featuredDrink = 'The JavaShake';
+}
+
